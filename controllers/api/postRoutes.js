d637e68df99e13c9b89da41f816be1c9eb4e09df
BearlyFunctional <hjbell123.hb@gmail.com>
2024-01-12 13:17:21 -0800

    added functionality for adding new posts. New script in makePost.js and postRoutes.js, as well as homepage, profile, and postbox.handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Post } = require('../../models');
 
 router.post('/newPost', async (req, res) => {
+    const post_title = req.body.post_title;
+    const post_body = req.body.post_body;
+    const user_id = req.session.user_id;
 
+    try {
+        const newPost = await Post.create({
+            post_title,
+            post_body,
+            user_id
+        })
+
+        res.status(200).json(newPost)
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }),
 
 module.exports = router;
--- public/js/makePost.js
+++ public/js/makePost.js
@@ -1,7 +1,23 @@
 const submitNewPost = async (event) => {
     event.preventDefault();
-    const postTitle = document.querySelector('#newPostTitle').value.trim();
-    const postContent = document.querySelector('#newPostBody').value.trim();
+    const post_title = document.querySelector('#newPostTitle').value.trim();
+    const post_body = document.querySelector('#newPostBody').value.trim();
+
+    if (post_title && post_body) {
+        const response = await fetch('/api/posts/newPost', {
+            method: 'POST',
+            body: JSON.stringify({ post_title, post_body }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            alert('Post created!');
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+
 }
 
 document
