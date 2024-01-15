498ce53ec6e73d68a97d76445b4338f7105bb523
Rob Gould <rgould.midwest@gmail.com>
2024-01-15 16:13:11 -0600

    adjusted user.js, user was User

--- models/User.js
+++ models/User.js
@@ -1,33 +1,31 @@
-const { getDefaultHighWaterMark } = require("stream");
-
 // models/user.js
 module.exports = (sequelize, DataTypes) => {
-const User = sequelize.define('User', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    age: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    location: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true, // Ensure email uniqueness
-        validate: {
-            isEmail: true // Validate email format
+    const User = sequelize.define('User', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true, // Ensure email uniqueness
+            validate: {
+                isEmail: true // Validate email format
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
-
-return User;
-};
+    });
+    
+    return User;
+    };
