1f0a8121b6a6f85cafbb9b4a08a58d979bf2fc82
Rob Gould <rgould.midwest@gmail.com>
2024-01-15 16:08:02 -0600

    adjusted index.js

--- models/index.js
+++ models/index.js
@@ -1,14 +1,24 @@
 const Sequelize = require('sequelize');
+let sequelize;
 
-// Initialize Sequelize with environment variables
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: 'mysql' // or another dialect
-    }
-);
+if (process.env.JAWSDB_URL) {
+	// Production environment (Heroku)
+	sequelize = new Sequelize(process.env.JAWSDB_URL, {
+		dialect: "mysql",
+	});
+} else {
+	// Development environment
+	sequelize = new Sequelize(
+		process.env.DB_DATABASE,
+		process.env.DB_USERNAME,
+		process.env.DB_PASSWORD,
+		{
+			host: process.env.DB_HOST,
+			dialect: "mysql",
+			port: process.env.DB_PORT || 3306,
+		}
+	);
+}
 
 const db = {};
 
@@ -17,14 +27,17 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // Import models and associate them with the Sequelize instance
-db.User = require('./user')(sequelize, Sequelize);
-db.Message = require('./message')(sequelize, Sequelize); // 
+db.User = require("./user")(sequelize, Sequelize);
+db.Message = require("./message")(sequelize, Sequelize); //
 
 // Set up associations
-db.User.hasMany(db.Message, { foreignKey: 'senderId', as: 'SentMessages' });
-db.Message.belongsTo(db.User, { foreignKey: 'senderId', as: 'Sender' });
+db.User.hasMany(db.Message, { foreignKey: "senderId", as: "SentMessages" });
+db.Message.belongsTo(db.User, { foreignKey: "senderId", as: "Sender" });
 
-db.User.hasMany(db.Message, { foreignKey: 'receiverId', as: 'ReceivedMessages' });
-db.Message.belongsTo(db.User, { foreignKey: 'receiverId', as: 'Receiver' });
+db.User.hasMany(db.Message, {
+	foreignKey: "receiverId",
+	as: "ReceivedMessages",
+});
+db.Message.belongsTo(db.User, { foreignKey: "receiverId", as: "Receiver" });
 
 module.exports = db;
