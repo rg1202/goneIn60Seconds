477c0925f5b1d97e4b12362a19393c26ac21360c
Rob Gould <rgould.midwest@gmail.com>
2024-01-17 00:36:56 -0600

    cleaning up

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const app = express(); // Create express app
 const server = http.createServer(app); // Create server using express app
 const io = socketIo(server); // Create socket using server
 
-// Apply middleware
 otherMiddleware.handlebars(app);  // Use handlebars middleware
 app.use(otherMiddleware.sessionMiddleware);  // Use session middleware
 app.use(otherMiddleware.bodyParserJson); // Use body parser middleware
@@ -17,11 +16,8 @@ app.use(otherMiddleware.bodyParserUrlencoded); // Use body parser middleware
 app.use(express.static("public")); // Serve static content for the app from the "public" directory in the application directory
 app.use(otherMiddleware.sessionMiddleware); // Use session middleware
 
-// Use routes
 app.use(routes); // Use routes
 
-
-// Socket.io chat controller
 const chatController = require('./controllers/chatController'); // Import chat controller
 io.on("connection", (socket) => { // Listen for socket.io connections
     chatController.onConnection(socket, io); // Call onConnection() in chat controller
